106ee40793f4914345da7286ee175d63e8dcc538
Moshe Levi <moshele@mellanox.com>
2015-04-13 22:43:49 +0300

    Adjustments for Kilo and update version to 2015.1.0
    Change-Id: I2f2154a3e9b6a081c60a7f997d53ef38f8d49616

--- mlnx_dhcp.py
+++ mlnx_dhcp.py
@@ -13,109 +13,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import abc
-import collections
-import os
-import re
-import shutil
-import socket
-import sys
-import uuid
-
 import netaddr
-from oslo.config import cfg
+from oslo_log import log as logging
 import six
 
 from neutron.agent.linux import dhcp
-from neutron.agent.linux import ip_lib
 from neutron.agent.linux import utils
-from neutron.common import constants
-from neutron.common import exceptions
-from neutron.openstack.common import importutils
-from neutron.openstack.common import jsonutils
-from neutron.openstack.common import log as logging
-from neutron.openstack.common import uuidutils
 
 LOG = logging.getLogger(__name__)
 
 
 class MlnxDnsmasq(dhcp.Dnsmasq):
 
-    def spawn_process(self):
-        """Spawns a Dnsmasq process for the network."""
-        env = {
-            self.NEUTRON_NETWORK_ID_KEY: self.network.id,
-        }
-
-        cmd = [
-            'dnsmasq',
-            '--no-hosts',
-            '--no-resolv',
-            '--strict-order',
-            '--bind-interfaces',
-            '--interface=%s' % self.interface_name,
-            '--except-interface=lo',
-            '--pid-file=%s' % self.get_conf_file_name(
-                'pid', ensure_conf_dir=True),
-            '--dhcp-hostsfile=%s' % self._output_hosts_file(),
-            '--addn-hosts=%s' % self._output_addn_hosts_file(),
-            '--dhcp-optsfile=%s' % self._output_opts_file(),
-            '--leasefile-ro',
-        ]
-
-        possible_leases = 0
-        for i, subnet in enumerate(self.network.subnets):
-            # if a subnet is specified to have dhcp disabled
-            if not subnet.enable_dhcp:
-                continue
-            if subnet.ip_version == 4:
-                mode = 'static'
-            else:
-                # TODO(mark): how do we indicate other options
-                # ra-only, slaac, ra-nameservers, and ra-stateless.
-                mode = 'static'
-            if self.version >= self.MINIMUM_VERSION:
-                set_tag = 'set:'
-            else:
-                set_tag = ''
-
-            cidr = netaddr.IPNetwork(subnet.cidr)
-
-            cmd.append('--dhcp-range=%s%s,%s,%s,%ss' %
-                       (set_tag, self._TAG_PREFIX % i,
-                        cidr.network,
-                        mode,
-                        self.conf.dhcp_lease_duration))
-            possible_leases += cidr.size
-
-        # Cap the limit because creating lots of subnets can inflate
-        # this possible lease cap.
-        cmd.append('--dhcp-lease-max=%d' %
-                   min(possible_leases, self.conf.dnsmasq_lease_max))
-
-        cmd.append('--conf-file=%s' % self.conf.dnsmasq_config_file)
-        if self.conf.dnsmasq_dns_servers:
-            cmd.extend(
-                '--server=%s' % server
-                for server in self.conf.dnsmasq_dns_servers)
-
-        if self.conf.dhcp_domain:
-            cmd.append('--domain=%s' % self.conf.dhcp_domain)
-
-        cmd.append('--dhcp-broadcast')
-
-        ip_wrapper = ip_lib.IPWrapper(self.root_helper,
-                                      self.network.namespace)
-        ip_wrapper.netns.execute(cmd, addl_env=env)
-
     def _output_hosts_file(self):
+        """Writes a dnsmasq compatible dhcp hosts file.
+
+        The generated file is sent to the --dhcp-hostsfile option of dnsmasq,
+        and lists the hosts on the network which should receive a dhcp lease.
+        Each line in this file is in the form::
+
+            'mac_address,FQDN,ip_address'
+
+        IMPORTANT NOTE: a dnsmasq instance does not resolve hosts defined in
+        this file if it did not give a lease to a host listed in it (e.g.:
+        multiple dnsmasq instances on the same network if this network is on
+        multiple network nodes). This file is only defining hosts which
+        should receive a dhcp lease, the hosts resolution in itself is
+        defined by the `_output_addn_hosts_file` method.
+        """
         prefix = 'ff:00:00:00:00:00:02:00:00:02:c9:00:'
         buf = six.StringIO()
         filename = self.get_conf_file_name('host')
 
-        LOG.debug(_('Building host file: %s'), filename)
+        LOG.debug('Building host file: %s', filename)
+        dhcp_enabled_subnet_ids = [s.id for s in self.network.subnets
+                                   if s.enable_dhcp]
         for (port, alloc, hostname, name) in self._iter_hosts():
-            set_tag = ''
+            if not alloc:
+                if getattr(port, 'extra_dhcp_opts', False):
+                    buf.write('%s,%s%s\n' %
+                              (port.mac_address, 'set:', port.id))
+                continue
+
+            # don't write ip address which belongs to a dhcp disabled subnet.
+            if alloc.subnet_id not in dhcp_enabled_subnet_ids:
+                continue
+
             # (dzyu) Check if it is legal ipv6 address, if so, need wrap
             # it with '[]' to let dnsmasq to distinguish MAC address from
             # IPv6 address.
@@ -123,25 +66,22 @@ class MlnxDnsmasq(dhcp.Dnsmasq):
             if netaddr.valid_ipv6(ip_address):
                 ip_address = '[%s]' % ip_address
 
-            LOG.debug(_('Adding %(mac)s : %(name)s : %(ip)s'),
-                      {"mac": port.mac_address, "name": name,
-                       "ip": ip_address})
             mac_first = port.mac_address[:8]
             middle = ':00:00:'
             mac_last = port.mac_address[9:]
             client_id = ''.join([prefix, mac_first, middle, mac_last])
 
             if getattr(port, 'extra_dhcp_opts', False):
-                if self.version >= self.MINIMUM_VERSION:
-                    set_tag = 'set:'
-
                 buf.write('%s,id:%s,%s,%s,%s%s\n' %
                           (port.mac_address, client_id, name, ip_address,
-                           set_tag, port.id))
+                           'set:', port.id))
             else:
                 buf.write('%s,id:%s,%s,%s\n' %
                           (port.mac_address, client_id, name, ip_address))
 
         utils.replace_file(filename, buf.getvalue())
-        LOG.debug(_('Done building host file %s'), filename)
+        LOG.debug('Done building host file %s with contents:\n%s', filename,
+                  buf.getvalue())
+        return filename
+
         return filename
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 Name = 'mlnx-dnsmasq'
 Url = ""
-Version = '2014.2.1'
+Version = '2015.1.0'
 License = 'Apache License 2.0'
 Author = 'Mellanox'
 AuthorEmail = 'openstack@mellanox.com'
