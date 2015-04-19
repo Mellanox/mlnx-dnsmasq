05cd497eb862c6e254a733f68ac69fe5fcd998c4
Moshe Levi <moshele@mellanox.com>
2015-04-19 11:43:04 +0300

    fix release old lease
    release_dhcp utils is used to release old leases
    for mlnx dhcp we need to update the methods to take
    the ip address and client id to release the lease
    
    Change-Id: I7fb93417891c1cb6853935781ea8ea3cf4982531

--- mlnx_dhcp.py
+++ mlnx_dhcp.py
@@ -13,11 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import netaddr
 from oslo_log import log as logging
 import six
 
 from neutron.agent.linux import dhcp
+from neutron.agent.linux import ip_lib
 from neutron.agent.linux import utils
 
 LOG = logging.getLogger(__name__)
@@ -84,4 +87,29 @@ class MlnxDnsmasq(dhcp.Dnsmasq):
                   buf.getvalue())
         return filename
 
-        return filename
+    def _release_lease(self, mac_address, ip, client_id):
+        """Release a DHCP lease."""
+        cmd = ['dhcp_release', self.interface_name, ip, mac_address, client_id]
+        ip_wrapper = ip_lib.IPWrapper(namespace=self.network.namespace)
+        ip_wrapper.netns.execute(cmd, run_as_root=True)
+
+    def _read_hosts_file_leases(self, filename):
+        leases = set()
+        if os.path.exists(filename):
+            with open(filename) as f:
+                for l in f.readlines():
+                    host = l.strip().split(',')
+                    leases.add((host[3].strip('[]'), host[0], host[1][3:]))
+        return leases
+
+    def _release_unused_leases(self):
+        filename = self.get_conf_file_name('host')
+        old_leases = self._read_hosts_file_leases(filename)
+
+        new_leases = set()
+        for port in self.network.ports:
+            for alloc in port.fixed_ips:
+                new_leases.add((alloc.ip_address, port.mac_address))
+
+        for ip, mac, client_id in old_leases - new_leases:
+            self._release_lease(mac, ip, client_id)
